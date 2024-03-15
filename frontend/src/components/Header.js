4448e77529b78120075fe9503413ff613b88dff2
Amritjot Kaur <amritjot.549@gmail.com>
2024-03-15 19:42:15 +0530

    Updated sidebar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import "./Header.css";
-import logo from "../assets/logo_blackk.png";
+import logo from "../assets/logo1.jpeg";
 import { FaBars, FaSearch} from "react-icons/fa";
 
 function Header() {
 
   const [isSidebarOpen , setIsSidebarOpen] = useState(false);
+  const [isExploreOpen, setIsExploreOpen] = useState(false);
 
   const toggleSidebar = () => {
     console.log("Hamburger menu clicked");
     setIsSidebarOpen(!isSidebarOpen);
+  };
+  const toggleExplore = () => {
+    setIsExploreOpen(!isExploreOpen);
   };
   
   return (
@@ -38,16 +42,32 @@ function Header() {
 
       {isSidebarOpen && (
         <div className="sidebar">
-
+          <div className="header__left">
+            <FaBars className="sidebar__menu" onClick={toggleSidebar} />
+            <img className="sidebar__logo" src={logo} alt="Logo" />
+          </div>
+          <div className="sidebar__list">
           <ul>
             <li><button><i class="bi bi-house"></i> HOME</button></li>
             <li><button><i class="bi bi-heart"></i> LIKED VIDEOS</button></li>
             <li><button><i className="bi bi-collection-play"></i> CHANNELS</button></li>
-            <li><button><i class="bi bi-fire"></i> EXPLORE</button></li>
+            <li><button onClick={toggleExplore}><i class="bi bi-fire"></i> EXPLORE</button></li>
+            <li>
+            {isExploreOpen && (
+          <div className="explore__list">
+            <ul>
+              <li>Item 1</li>
+              <li>Item 1</li>
+              <li>Item 1</li>
+            </ul>
+          </div>
+        )}
+            </li>
             <li><button><i class="bi bi-chat-square-text"></i> SEND FEEDBACK</button></li>
             <li><button><i class="bi bi-info-circle"></i> ABOUT</button></li>
           </ul>
-
+        
+        </div>
         </div>
       )}
     </div>
