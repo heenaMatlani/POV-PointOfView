de66af2b28db5fd4c2637ae67b895dec958df747
Aarushi Singh <aarushi.singh21b@iiitg.ac.in>
2024-03-16 22:17:42 +0530

    formatting of homepage

--- frontend/src/components/Recommended.js
+++ frontend/src/components/Recommended.js
@@ -1,122 +1,52 @@
 import React from "react";
 import "./Recommended.css";
 import thumbnail from "../assets/thumbnail.png";
-import channel from "../assets/channel.png";
+import Card from "./Card";
 
 function Recommended() {
+  let channel =
+    "https://heena0708.s3.ap-south-1.amazonaws.com/icons/ABPNews.jpeg";
+  let d =
+    "Some quick example text to build on the card title and make up the bulk of the card content.";
   return (
     <div className="recommended">
       <div className="container text-center">
         <div className="row">
-          {/* First Row */}
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src= "https://heena0708.s3.ap-south-1.amazonaws.com/icons/ABPNews.jpeg"className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Second Row */}
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="card">
-              <img src={thumbnail} className="card-img-top menuu" alt="ima" />
-              <div className="card-body">
-                <img src={channel} className="channel" alt="channel" />
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <Card thumbnail={thumbnail} channel={channel} description={d} />
           </div>
         </div>
       </div>
