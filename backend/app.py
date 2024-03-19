a6c70b5c005935bd782c2e527128ba84d315cd0a
heenaMatlani <heenamatlani07@gmail.com>
2024-03-19 11:01:12 +0530

    Added backend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
-import flask
+from flask import Flask, jsonify
+import pymysql
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+db_host = 'localhost'
+db_user = 'root'
+db_password = 'pass'
+db_name = 'pov'
+
+connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+@app.route('/homepage')
+def get_videos():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM videos")
+    videos = cursor.fetchall()
+    cursor.close()
+    return jsonify(videos)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
