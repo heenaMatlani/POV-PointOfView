4f07f547bc6b8f177eb60775332962973e42a0af
heenaMatlani <heenamatlani07@gmail.com>
2024-03-19 11:03:57 +0530

    Added backend to recommended

--- frontend/src/components/Recommended.js
+++ frontend/src/components/Recommended.js
@@ -1,113 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Recommended.css";
-import thumbnail from "../assets/thumbnail.png";
 import Card from "./Card.js";
 import { Link } from "react-router-dom";
 
 function Recommended() {
-  let channel =
-    "https://heena0708.s3.ap-south-1.amazonaws.com/icons/ABPNews.jpeg";
-  let d =
-    "Some quick example text to build on the card title and make up the bulk of the card content.";
+    const [videos, setVideos] = useState([]);
 
-  let vid = "https://www.youtube.com/watch?v=sjLDT-hLkFQ";
+    useEffect(() => {
+        fetch('/homepage')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+            .catch(error => console.log(error));
+    }, []);
+
     return (
     <div className="recommended">
       <div className="container text-center">
         <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <Link className="video-link" to ="/video"><Card className="recommended-card" video={vid} thumbnail={thumbnail} channel={channel} description={d}/></Link>
-          </div>
-          
-          
+                    {videos.map(video => (
+                        <div key={video.video_id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                            <Link className="video-link" to="/video">
+                                <Card
+                                    className="recommended-card"
+                                    video={video.video_url}
+                                    thumbnail={video.video_thumbnail}
+                                    channel={video.channel_id}
+                                    description={video.title}
+                                />
+                            </Link>
+                        </div>
+                    ))}
         </div>
       </div>
     </div>
