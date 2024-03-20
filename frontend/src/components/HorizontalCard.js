dd3e78604a0c51cd60f4e63538a064ff53127d81
Aarushi Singh <aarushi.singh21b@iiitg.ac.in>
2024-03-20 20:24:09 +0530

    built search page

--- frontend/src/components/HorizontalCard.js
+++ frontend/src/components/HorizontalCard.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import './HorizontalCard.css'
+import React from "react";
+import "./HorizontalCard.css";
 function HorizontalCard(props) {
   return (
     <div className="hcard">
-     <div className="card">
-      <img src={props.thumbnail} className="card-img-top menuu" alt="ima" />
-      <div className="card-body">
-        <img src={props.channel} className="channel" alt="channel" />
-        <p className="card-text">{props.description}</p>
+      <div className="hcard__thumbnail">
+        <img src={props.thumbnail} className="hcard__img" alt="thumbnail" />
+      </div>
+      <div className="hcard__description">
+        <p className="hcard__text">{props.title}</p>
+        <div className="hcard__details">
+          <p className="hcard__views">{props.views} lakh views •</p>
+          <p className="hcard__age">{props.age} months ago</p>
+        </div>
+        <div className="hcard__details">
+          <img src={props.channel} className="hcard__channel" alt="channel" />
+          <p className="hcard__channelName">{props.channelName}</p>
+        </div>
+        <p className="hcard__channelDescription">{props.channelDescription}</p>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default HorizontalCard
+export default HorizontalCard;
--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -3,6 +3,7 @@ import './SearchPage.css'
 import thumbnail from "../assets/thumbnail.png";
 import Header from './Header'
 import HorizontalCard from './HorizontalCard'
+import { Link } from 'react-router-dom';
 function SearchPage() {
     let channel =
     "https://heena0708.s3.ap-south-1.amazonaws.com/icons/ABPNews.jpeg";
@@ -10,17 +11,28 @@ function SearchPage() {
     "Some quick example text to build on the card title and make up the bulk of the card content.";
 
   let vid = "https://www.youtube.com/watch?v=sjLDT-hLkFQ";
+
+  let views = "12"
+
+  let age = "13"
+
+  let cn="ABP News"
+
+  let cd="Buying and trying candy, snacks, makeup, hair products, clothing and more created by YOUTUBERS. Are they worth buying?!?"
   return (
-    <div>
+    <div className='search'>
       <Header/>
-      <h1>searched me??</h1>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      <HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} description={d}/>
-      
+      <div className="search__content">
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      <Link className='search__link' to="/video"><HorizontalCard video={vid} thumbnail={thumbnail} channel={channel} title={d} views={views} age={age} channelName={cn} channelDescription={cd}/></Link>
+      </div>
     </div>
   )
 }
