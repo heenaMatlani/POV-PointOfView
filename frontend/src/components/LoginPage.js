89388a257582809e11b93d8ad61f2b36e85baa11
Amritjot Kaur <amritjot.549@gmail.com>
2024-03-21 11:41:10 +0530

    Updated Login Page

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,15 +1,50 @@
 // LoginPage.js
-import thumbnail from "../assets/thumbnail.png";
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import './LoginPage.css'
+import { FaUser,FaLock } from 'react-icons/fa';
 const LoginPage = () => {
+  const [isSignup, setIsSignUp] = useState(false);
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const toggleSignup = () => {
+    setIsSignUp(!isSignup);
+  };
   return (
-    <div>
-      <h2>Login Page</h2>
-      <Link to="/homepage">
+    <div className='wrapper'>
+      {/* <Link to="/homepage">
         <button>Login</button>
-      </Link>
+      </Link> */}
+      <form action="">
+        <h1>{isSignup ?  'Sign Up' : 'Login'}</h1>
+        <div className='input-box'>
+        <input type='text' placeholder='E-mail' required value={email}/>
+        <FaUser className='login__icon'/>
+        </div>
+        <div className='input-box'>
+        <input type='password' placeholder='Password' required value={password}/>
+        <FaLock className='login__icon'/>
+        </div>
+
+
+
+        <Link to="/homepage">
+        {/* <button type='submit'>Login</button> */}
+        <button type='submit'>{isSignup ? 'Sign Up' : 'Login'}</button>
+        </Link> 
+        <div className='remember-forgot'>
+          <label>
+          <input type='checkbox'/> Remember me
+          </label>
+          <a href='#'>Forgot Password?</a>
+        </div>
+        <div className='register-link'>
+          {/* <p>Don't have an account?<a href="#">Sign Up</a></p> */}
+          <p>{isSignup ? 'Already have an account?' : "Don't have an account?"}<a href="#" onClick={toggleSignup}>{isSignup ? 'Login' : 'Sign Up'}</a></p>
+        </div>
+      </form>
+    
+      
     </div>
   );
 };
