ffcf16c0415f651d399d8d99ff91c99e11e93d24
Amritjot Kaur <amritjot.549@gmail.com>
2024-03-22 22:15:02 +0530

    Updated hamburger menu option

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-def establish_connection(host='localhost', user='root', passwd='matlani01k', database='pov'):
+def establish_connection(host='localhost', user='root', passwd='Amritjot@1232', database='pov'):
     """Establishes connection with local database, throws exception(not error) if connection not established"""
     import mysql.connector as cntr
     from mysql.connector import Error
